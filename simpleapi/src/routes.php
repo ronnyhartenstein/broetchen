386de5028ecef24bf9bbfb9ccd46cefe6e84dd74
Ronny Hartenstein <ronny@rh-flow.de>
2017-10-29 13:30:42 +0100

    Orders GET+POST

--- simpleapi/src/routes.php
+++ simpleapi/src/routes.php
@@ -7,14 +7,11 @@ $app->group('/api', function () use ($app) {
         $params = json_decode(file_get_contents('php://input'), true);
 //        dump($params);
         if (empty($params['email_address']) || empty($params['password'])) {
-            return $response->withJson(['error' => 'Email or Pwd empty'], 401);
+            return $response->withJson(['error' => 'Email or Pwd empty'], 400);
         }
-        $users = json_decode(file_get_contents(ROOT_DIR.'/db/users.json'), true);
-        $myusers = array_filter($users, function($n) use ($params) {
-            return $n['email'] == $params['email_address'] && $n['password'] == $params['password'];
-        });
-        if (count($myusers) == 1) {
-            $myuser = $myusers[0];
+        $user = new \App\User;
+        $myuser = $user->isValid($params['email_address'], $params['password']);
+        if ($myuser) {
             return $response->withJson(['sessionid' => md5($myuser['email'])]);
         } else {
             return $response->withJson(['error' => 'User not found'], 401);
@@ -26,11 +23,39 @@ $app->group('/api', function () use ($app) {
         return $response->withJson($services);
     });
 
-    $app->get('/orders', function (ServerRequestInterface $request, ResponseInterface $response) {
+    $app->get('/orders/{session}', function (ServerRequestInterface $request, ResponseInterface $response, $params) {
+        if (empty($params['sessionid'])) {
+            return $response->withJson(['error' => 'Session needed'], 400);
+        }
+        $user = new \App\User;
+        $myuser = $user->getBySession($params['sessionid']);
+        if (!$myuser) {
+            return $response->withJson(['error' => 'User not found'], 401);
+        }
+
+        $json = ROOT_DIR.'db/orders-'.$myuser['email'].'.json';
+        if (!file_exists($json)) {
+            return $response->withJson([]);
+        }
+        $orders = json_decode(file_get_contents($json), true);
+        return $response->withJson($orders);
 
     });
 
     $app->post('/orders', function (ServerRequestInterface $request, ResponseInterface $response) {
-        $oders = json_decode(file_get_contents('php://input'), true);
+        $params = json_decode(file_get_contents('php://input'), true);
+        if (empty($params['sessionid'])) {
+            return $response->withJson(['error' => 'Session needed'], 400);
+        }
+        if (empty($params['orders'])) {
+            return $response->withJson(['error' => 'Orders needed'], 400);
+        }
+        $user = new \App\User;
+        $myuser = $user->getBySession($params['sessionid']);
+        if (!$myuser) {
+            return $response->withJson(['error' => 'User not found'], 401);
+        }
+        $json = ROOT_DIR.'db/orders-'.$myuser['email'].'.json';
+        file_put_contents($json, json_encode($params['orders'], JSON_PRETTY_PRINT));
     });
 });
