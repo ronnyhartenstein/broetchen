d8cc8e399776be4c5a6f7b0ce73e695da512c77c
Ronny Hartenstein <ronny@rh-flow.de>
2017-11-23 23:26:10 +0100

    Simple Frontend mit jQuery \o/

--- simple/src/routes.php
+++ simple/src/routes.php
@@ -1,12 +1,21 @@
 <?php
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use App\User;
 
+$app->get('/', function (Request $request, Response $response) {
+   $response->getBody()->write(file_get_contents('frontend.html'));
+   return $response;
+});
+
 $app->group('/api', function () use ($app) {
-    $app->post('/login', function (ServerRequestInterface $request, ResponseInterface $response) {
-        $params = json_decode(file_get_contents('php://input'), true);
-//        dump($params);
+
+    // curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d "{\"email_address\":\"bla@foo.bar\", \"password\":\"xxx\"}" http://localhost:3000/api/login
+    $app->post('/login', function (Request $request, Response $response) {
+        $input = file_get_contents('php://input');
+        $params = json_decode($input, true);
+        //print json_last_error_msg();
+        //print_r($input); print "\n"; print_r($params); die();
         if (empty($params['email_address']) || empty($params['password'])) {
             return $response->withJson(['error' => 'Email or Pwd empty'], 400);
         }
@@ -19,7 +28,8 @@ $app->group('/api', function () use ($app) {
         }
     });
 
-    $app->get('/services', function (ServerRequestInterface $request, ResponseInterface $response) {
+    // curl http://localhost:3000/api/services
+    $app->get('/services', function (Request $request, Response $response) {
         $file = ROOT_DIR.'/db/services.json';
         if (!file_exists($file)) {
             copy($file.'.dist', $file);
@@ -28,7 +38,8 @@ $app->group('/api', function () use ($app) {
         return $response->withJson($services);
     });
 
-    $app->get('/orders/{sessionid}', function (ServerRequestInterface $request, ResponseInterface $response, $params) {
+    // curl http://localhost:3000/api/orders/4406a33260d8956e2d95fae136a5ea74
+    $app->get('/orders/{sessionid}', function (Request $request, Response $response, $params) {
         if (empty($params['sessionid'])) {
             return $response->withJson(['error' => 'Session needed'], 400);
         }
@@ -47,7 +58,8 @@ $app->group('/api', function () use ($app) {
 
     });
 
-    $app->post('/orders', function (ServerRequestInterface $request, ResponseInterface $response) {
+    // curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d "{\"sessionid\":\"4406a33260d8956e2d95fae136a5ea74\", \"orders\": {\"broetchen\":{\"schrippe\":3,\"vollkornbrot\":1}}}" http://localhost:3000/api/orders
+    $app->post('/orders', function (Request $request, Response $response) {
         $params = json_decode(file_get_contents('php://input'), true);
         if (empty($params['sessionid'])) {
             return $response->withJson(['error' => 'Session needed'], 400);
